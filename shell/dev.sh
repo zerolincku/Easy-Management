acd18f76c1c2ae58e7d59db4c83139b827b89abd
林常坤 <zrezerolinck@foxmail.com>
2020-10-13 14:50:57 +0800

    fix

--- shell/dev.sh
+++ shell/dev.sh
@@ -5,5 +5,5 @@ ls /
 # date >> log
 # exit
 # remotessh
-# scp -P 22 target/management-0.0.1.jar root@119.3.159.234:/home/lin
+#   scp -P 22 target/management-0.0.1.jar root@119.3.159.234:/home/lin
 ssh -p 22 root@119.3.159.234 "echo SUCCESS"
