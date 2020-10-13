bd57088b2e1c00dc75451ecde7179e436b2523bc
林常坤 <zrezerolinck@foxmail.com>
2020-10-13 14:57:53 +0800

    fix

--- README.md
+++ README.md
@@ -24,3 +24,5 @@ Easy-Management 基于SpringBoot、Spring Security实现的前后端分离的权
 
 
 基于Vue的前端项目，正在开发中.....
+
+最新提交
--- shell/dev.sh
+++ shell/dev.sh
@@ -1,9 +1,10 @@
 pwd
 ls /
-# ssh -p 22 -Tq root@119.3.159.234 << remotessh
-# cd /home/lin
-# date >> log
-# exit
-# remotessh
-#   scp -P 22 target/management-0.0.1.jar root@119.3.159.234:/home/lin
-ssh -p 22 root@119.3.159.234 "echo SUCCESS"
+ssh -p 22 -Tq root@119.3.159.234 << remotessh
+cd /home/lin
+date >> log
+rm -f README.md
+exit
+remotessh
+scp -P 22 target/README.md root@119.3.159.234:/home/lin
+echo SUCCESS!!
