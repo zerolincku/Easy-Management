15059f7200f4877211408dbe5e20f8f8b617e0cd
林常坤 <zrezerolinck@foxmail.com>
2020-10-14 11:16:39 +0800

    add maven profile setting

--- src/main/java/com/linck/management/system/controller/VerificationCodeController.java
+++ src/main/java/com/linck/management/system/controller/VerificationCodeController.java
@@ -9,9 +9,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import sun.rmi.runtime.Log;
 
 import javax.imageio.ImageIO;
 import javax.servlet.ServletOutputStream;
