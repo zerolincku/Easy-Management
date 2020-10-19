142a8b030d09b072301ad13a3f824fb7ed561c90
Linck <zerolinck@foxmail.com>
2020-10-20 00:25:05 +0800

    modify

--- src/main/java/com/linck/management/system/controller/UserController.java
+++ src/main/java/com/linck/management/system/controller/UserController.java
@@ -1,6 +1,5 @@
 package com.linck.management.system.controller;
 
-import com.google.code.kaptcha.Constants;
 import com.linck.management.common.api.CommonResult;
 import com.linck.management.system.contants.SysPermissionTypeEnum;
 import com.linck.management.system.dto.LoginUserDTO;
@@ -19,7 +18,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.transaction.annotation.Transactional;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -54,25 +52,24 @@ public class UserController {
 
     @ApiOperation("用户登录")
     @PostMapping("/login")
-    public CommonResult login(@RequestBody @Validated LoginUserDTO userDetail, HttpServletRequest request){
+    public CommonResult login(@RequestBody @Validated LoginUserDTO userDetail, HttpServletRequest request) {
         /*if(!userDetail.getVerificationCode().equals(request.getSession().getAttribute(Constants.KAPTCHA_SESSION_KEY))){
             return CommonResult.validateFailed("验证码错误");
         }*/
         String token = sysUserService.login(userDetail);
-        if(token == null){
+        if (token == null) {
             return CommonResult.validateFailed("用户名或密码错误");
         }
         Map<String, String> tokenMap = new HashMap<>(2);
-        tokenMap.put("token", token);
-        tokenMap.put("tokenHead", tokenHead);
+        tokenMap.put("token", tokenHead + " " + token);
         return CommonResult.success(tokenMap);
     }
 
     @ApiOperation("用户注册")
     @PostMapping("/register")
-    public CommonResult register(@RequestBody @Validated LoginUserDTO userDetail){
+    public CommonResult register(@RequestBody @Validated LoginUserDTO userDetail) {
         SysUser sysUser = sysUserService.register(userDetail);
-        if(sysUser == null){
+        if (sysUser == null) {
             return CommonResult.failed("该用户名已经被注册");
         }
         sysUser.setPwd(null);
@@ -83,7 +80,7 @@ public class UserController {
     // @PreAuthorize("hasAuthority('user:view')")
     @ApiOperation("查询用户菜单和按钮")
     @PostMapping("/query/menu")
-    public CommonResult<List<SysPermissionVO>> queryPermission(){
+    public CommonResult<List<SysPermissionVO>> queryPermission() {
         // 从SecurityContextHolder获取当前用户
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         SysUserDetails sysUserDetails = (SysUserDetails) authentication.getPrincipal();
@@ -92,9 +89,9 @@ public class UserController {
         List<SysPermission> sysPermissions = sysPermissionService.listByUserId(sysUser.getId());
         // 封装菜单和按钮返回前端
         List<SysPermissionVO> resultPermissions = new ArrayList<>();
-        sysPermissions.forEach( sysPermission -> {
+        sysPermissions.forEach(sysPermission -> {
             // 权限行不返回前端
-            if(!sysPermission.getType().equals(SysPermissionTypeEnum.PERMISSION.getType())){
+            if (!sysPermission.getType().equals(SysPermissionTypeEnum.PERMISSION.getType())) {
                 SysPermissionVO sysPermissionVO = new SysPermissionVO();
                 BeanUtils.copyProperties(sysPermission, sysPermissionVO);
                 resultPermissions.add(sysPermissionVO);
