9dccebd501b78968a3bf261cc79c4795edac3ff9
linck <zerolinck@foxmail.com>
2021-11-11 00:00:55 +0800

    修改测试 Job

--- src/main/java/com/linck/management/quartz/job/TestJob.java
+++ src/main/java/com/linck/management/quartz/job/TestJob.java
@@ -1,5 +1,6 @@
 package com.linck.management.quartz.job;
 
+import lombok.extern.slf4j.Slf4j;
 import org.quartz.Job;
 import org.quartz.JobExecutionContext;
 import org.quartz.JobExecutionException;
@@ -10,9 +11,10 @@ import org.quartz.JobExecutionException;
  * @author: linck
  * @create: 2020-10-13 21:44
  **/
+@Slf4j
 public class TestJob implements Job {
     @Override
     public void execute(JobExecutionContext jobExecutionContext) throws JobExecutionException {
-
+        log.info("TestJob 执行");
     }
 }
