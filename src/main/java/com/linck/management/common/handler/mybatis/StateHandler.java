6cfe3d6609e9549ba6a8eabe8a8482c77a56cda6
linck <zerolinck@foxmail.com>
2021-11-29 20:52:35 +0800

    state 字段使用 mybatis 序列化为枚举类型

---
+++ src/main/java/com/linck/management/common/handler/mybatis/StateHandler.java
@@ -0,0 +1,36 @@
+package com.linck.management.common.handler.mybatis;
+
+import com.linck.management.common.model.constant.StateEnum;
+import org.apache.ibatis.type.BaseTypeHandler;
+import org.apache.ibatis.type.JdbcType;
+
+import java.sql.CallableStatement;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+/**
+ * @author: linck
+ * @create: 2021-11-24
+ */
+public class StateHandler extends BaseTypeHandler<StateEnum> {
+    @Override
+    public void setNonNullParameter(PreparedStatement preparedStatement, int i, StateEnum stateEnum, JdbcType jdbcType) throws SQLException {
+        preparedStatement.setInt(i, stateEnum.getValue());
+    }
+
+    @Override
+    public StateEnum getNullableResult(ResultSet resultSet, String s) throws SQLException {
+        return StateEnum.getStateEnum(resultSet.getInt(s));
+    }
+
+    @Override
+    public StateEnum getNullableResult(ResultSet resultSet, int i) throws SQLException {
+        return StateEnum.getStateEnum(resultSet.getInt(i));
+    }
+
+    @Override
+    public StateEnum getNullableResult(CallableStatement callableStatement, int i) throws SQLException {
+        return StateEnum.getStateEnum(callableStatement.getInt(i));
+    }
+}
--- src/main/java/com/linck/management/common/model/constant/StateEnum.java
+++ src/main/java/com/linck/management/common/model/constant/StateEnum.java
@@ -1,6 +1,12 @@
 package com.linck.management.common.model.constant;
 
+import com.fasterxml.jackson.annotation.JsonValue;
+import com.linck.management.common.api.ResultCodeEnum;
+import com.linck.management.common.exception.CustomException;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
 
 /**
  * @program: MyManagement
@@ -8,21 +14,33 @@ import lombok.Getter;
  * @author: linck
  * @create: 2020-08-09 17:20
  **/
+@Slf4j
 @Getter
+@AllArgsConstructor
 public enum StateEnum {
 
     /**
      * 启用
      */
-    ENABLE(1),
+    ENABLE(1, "启用"),
     /**
      * 禁用
      */
-    DISABLE(0);
+    DISABLE(0, "禁用");
 
+    @JsonValue
     private Integer value;
+    
+    private String name;
 
-    StateEnum(Integer value) {
-        this.value = value;
+    @SneakyThrows
+    public static StateEnum getStateEnum(int value) {
+        for (StateEnum stateEnum : StateEnum.values()) {
+            if (stateEnum.value == value) {
+                return stateEnum;
+            }
+        }
+        log.warn("没有对应的 State，value: {}", value);
+        throw new CustomException(ResultCodeEnum.NO_STATE_VALUE, "StateEnum.getStateEnum");
     }
 }
